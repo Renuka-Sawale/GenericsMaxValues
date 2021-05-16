c0c42aaeca0ab9beb2c7c0d7b8715d631031fc71
Renuka Sawale <rrsuryawanshi11@gmail.com>
2021-05-16 21:02:05 +0530

    Added Generics to find maximum numbers for integer, double and string

--- src/test/java/com/generics/MaximumNoTest.java
+++ src/test/java/com/generics/MaximumNoTest.java
@@ -8,63 +8,63 @@ public class MaximumNoTest {
     @Test
     public void givenIntegerValueMaxAtFirstPosition() {
         FindMaximumNumber maximumNo = new FindMaximumNumber();
-        Integer result = maximumNo.DisplayIntegerMaximumNo(15, 10, 5);
+        Integer result = maximumNo.DisplayMaximumNo(15, 10, 5);
         Assertions.assertEquals(15, result);
     }
 
     @Test
     public void givenIntegerValueMaxAtSecondPosition() {
         FindMaximumNumber maximumNo = new FindMaximumNumber();
-        Integer result = maximumNo.DisplayIntegerMaximumNo(5, 15, 10);
+        Integer result = maximumNo.DisplayMaximumNo(5, 15, 10);
         Assertions.assertEquals(15, result);
     }
 
     @Test
     public void givenIntegerValueMaxAtThirdPosition() {
         FindMaximumNumber maximumNo = new FindMaximumNumber();
-        Integer result = maximumNo.DisplayIntegerMaximumNo(5, 10, 15);
+        Integer result = maximumNo.DisplayMaximumNo(5, 10, 15);
         Assertions.assertEquals(15, result);
     }
 
     @Test
     public void givenDoubleValueMaxAtFirstPosition() {
         FindMaximumNumber maximumNo = new FindMaximumNumber();
-        Double result = maximumNo.DisplayDoubleMaximumNo(15.2, 10.2, 5.2);
+        Double result = maximumNo.DisplayMaximumNo(15.2, 10.2, 5.2);
         Assertions.assertEquals(15.2, result);
     }
 
     @Test
     public void givenDoubleValueMaxAtSecondPosition() {
         FindMaximumNumber maximumNo = new FindMaximumNumber();
-        Double result = maximumNo.DisplayDoubleMaximumNo(5.2, 15.2, 10.2);
+        Double result = maximumNo.DisplayMaximumNo(5.2, 15.2, 10.2);
         Assertions.assertEquals(15.2, result);
     }
 
     @Test
     public void givenDoubleValueMaxAtThirdPosition() {
         FindMaximumNumber maximumNo = new FindMaximumNumber();
-        Double result = maximumNo.DisplayDoubleMaximumNo(5.2, 10.2, 15.2);
+        Double result = maximumNo.DisplayMaximumNo(5.2, 10.2, 15.2);
         Assertions.assertEquals(15.2, result);
     }
 
     @Test
     public void givenStringValueMaxAtFirstPosition() {
         FindMaximumNumber maximumNo = new FindMaximumNumber();
-        String result = maximumNo.DisplayStringMaximumNo("Peach", "Apple", "Banana");
+        String result = maximumNo.DisplayMaximumNo("Peach", "Apple", "Banana");
         Assertions.assertEquals("Peach", result);
     }
 
     @Test
     public void givenStringValueMaxAtSecondPosition() {
         FindMaximumNumber maximumNo = new FindMaximumNumber();
-        String result = maximumNo.DisplayStringMaximumNo("Apple", "Peach", "Banana");
+        String result = maximumNo.DisplayMaximumNo("Apple", "Peach", "Banana");
         Assertions.assertEquals("Peach", result);
     }
 
     @Test
     public void givenStringValueMaxAtThirdPosition() {
         FindMaximumNumber maximumNo = new FindMaximumNumber();
-        String result = maximumNo.DisplayStringMaximumNo("Apple", "Banana", "Peach");
+        String result = maximumNo.DisplayMaximumNo("Apple", "Banana", "Peach");
         Assertions.assertEquals("Peach", result);
     }
 }
